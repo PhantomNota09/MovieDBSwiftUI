4ddacd85c18250852022f4b672e23da132b303e3
PhantomNota09 <srayala5@asu.edu>
2026-02-18 13:19:43 -0700

    Initial Commit

---
+++ MovieDBSwiftUI/MovieDBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBSwiftUIApp.swift
+//  MovieDBSwiftUI
+//
+//  Created by Surya Rayala on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
